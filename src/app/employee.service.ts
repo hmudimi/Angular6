195ca6fc22cef31ed1ae3e1732fe77ea8a22673d
amir.hussain <amir.hussain@mobigesture.com>
2018-10-15 15:08:21 -0700

    sample

---
+++ src/app/employee.service.ts
@@ -0,0 +1,18 @@
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { IEmployee } from './employee';
+import { Observable } from 'rxjs';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class EmployeeService {
+
+  private _url = "/assets/data/employee.json";
+
+  constructor(private http: HttpClient) { }
+
+  public getEmployees():Observable<IEmployee[]> {
+    return this.http.get<IEmployee[]>(this._url);
+  }
+}
